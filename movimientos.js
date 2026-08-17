6c53c210f29baa7ff7ff9c753c828fe0df8385ba
diesuno <85799687+diesuno@users.noreply.github.com>
2026-08-17 10:13:38 -0300

    Change condition for expense evaluation

--- movimientos.js
+++ movimientos.js
@@ -9,7 +9,7 @@ import { guardarDatosEnNube } from './auth.js';
 
 export function evaluarCamposDinamicosGasto() {
     let t = document.getElementById('inputTipo').value;
-    let esGasto = (t !== "Gasto Variable");
+    let esGasto = (t !== "ingreso");
     let esAvanzado = (estadoApp.perfilUsuario.modo === "AVANZADO");
 
     if (!esGasto || !esAvanzado) {
